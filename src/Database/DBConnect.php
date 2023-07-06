df6ef24015cbecf2f4a52f3fde17b872ab3fe142
laurent <laurent.last7@gmail.com>
2023-07-06 11:59:07 +0200

    database ok

---
+++ src/Database/DBConnect.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database;
+
+use PDO;
+
+final class DBConnect
+{
+    private static ?PDO $pdo = null;
+
+    public static function getPDO(): PDO
+    {
+        if (self::$pdo === null) {
+            self::$pdo = new PDO(DATABASE_DNS, DATABASE_USER, DATABASE_PASSWORD);
+        }
+        return self::$pdo;
+    }
+}
